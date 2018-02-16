b902ed8e063605fd82dd57a19800f1e7c4ac3ac7
Guillaume GAUTREAU <ggautreau@users.noreply.github.com>
2018-02-16 17:47:32 +0100

    fix version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     setup(
         name = name,
-        version = "0.0.2",
+        version = "0.0.3",
         author = "Guillaume GAUTREAU",
         author_email = "ggautrea@genoscope.cns.fr",
         description = "Depict microbial diversity via a partitioned pangenome graph",
