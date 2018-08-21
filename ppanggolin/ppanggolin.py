09b8a3cac4419ced9e3004b0c34399b0041c3872
Guillaume <guillaume.gautreau@free.fr>
2018-08-21 18:31:50 +0200

    small encoding bug

--- ppanggolin/ppanggolin.py
+++ ppanggolin/ppanggolin.py
@@ -337,7 +337,7 @@ class PPanGGOLiN:
             if len(values) > force_max_value:
                 ret+="\n"
                 ret+="And "+str(sum(values[50:]))+" nodes having degree above "+str(force_max_value)+"..."
-            return(u'↵'.encode('utf-8')+ret)
+            return(ret)
 
         pan_str ="\n"
         pan_str += "----------- Statistics -----------\n"
