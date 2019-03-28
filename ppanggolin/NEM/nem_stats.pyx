606460e38230ea1f408c965bba148f41ff960b01
Guillaume <guillaume.gautreau@free.fr>
2019-03-28 15:11:20 +0100

    rename nem to nem_stats

---
+++ ppanggolin/NEM/nem_stats.pyx
@@ -0,0 +1,17 @@
+cdef extern from "nem_exe.h":
+   cpdef int nem(const char* Fname,
+                 const int   nk,
+                 const char* algo,
+                 const float beta,
+                 const char* convergence,
+                 const float convergence_th,
+                 const char* format,
+                 const int   it_max,
+                 const int   dolog,
+                 const char* model_family,
+                 const char* proportion,
+                 const char* dispersion,
+                 const int   init_mode,
+                 const char* init_file,
+                 const char* out_file_prefix,
+                 const int   seed);
