9dab059e180f8d2c35dc807065a5f93d4bec14f9
Adelme Bazin <abazin@genoscope.cns.fr>
2019-06-03 11:47:29 +0200

    In the very weird case where there is no persistent, do not evaluate the length of single_copy_markers when projecting the partition on genomes (as it is set to None by default, that raised an error)

--- ppanggolin/ppanggolin.py
+++ ppanggolin/ppanggolin.py
@@ -2593,10 +2593,10 @@ class PPanGGOLiN:
         if self.is_partitionned:
             single_copy_markers = None
             if len(self.partitions["persistent"])>0:
-                 duplications        = {node : [len(data[org]) for org in (set(data) & set(organisms_to_project))] for node, data in self.neighbors_graph.subgraph(self.partitions["persistent"]).nodes(data=True)}
-                 mean_duplication    = {node : mean(occurences) for node, occurences in duplications.items()}
-                 single_copy_markers = {node : value for node, value in mean_duplication.items() if value < 1+duplication_margin}
-            single_copy_markers = None if len(single_copy_markers) == 0 else single_copy_markers 
+                duplications        = {node : [len(data[org]) for org in (set(data) & set(organisms_to_project))] for node, data in self.neighbors_graph.subgraph(self.partitions["persistent"]).nodes(data=True)}
+                mean_duplication    = {node : mean(occurences) for node, occurences in duplications.items()}
+                single_copy_markers = {node : value for node, value in mean_duplication.items() if value < 1+duplication_margin}
+                single_copy_markers = None if len(single_copy_markers) == 0 else single_copy_markers 
             with open(out_dir+"/nb_genes.csv","w") as nb_genes_file:
                 nb_genes_file.write(sep.join(["org","nb_persistent_families","nb_shell_families","nb_cloud_families","nb_exact_core_families","nb_exact_accessory_families","nb_soft_core_families","nb_soft_accessory_families","nb_gene_families","nb_persistent_genes","nb_shell_genes","nb_cloud_genes","nb_exact_core_genes","nb_exact_accessory_genes","nb_soft_core_genes","nb_soft_core_genes","nb_pangenome_genes","completeness","strain_contamination","nb_single_copy_markers"])+"\n")
                 for organism in organisms_to_project:
