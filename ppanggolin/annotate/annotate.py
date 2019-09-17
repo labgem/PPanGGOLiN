f49de0a3f6c03cae69e1fff694a1614051c7799e
Adelme Bazin <abazin@genoscope.cns.fr>
2019-09-17 16:56:24 +0200

    added an exception if the provided organisms file is empty

--- ppanggolin/annotate/annotate.py
+++ ppanggolin/annotate/annotate.py
@@ -310,7 +310,8 @@ def annotatePangenome(pangenome, fastaList, tmpdir, cpu, translation_table="11",
             logging.getLogger().error("No tabulation separator found in organisms file")
             exit(1)
         arguments.append((elements[0], elements[1], elements[2:], translation_table, kingdom, norna, tmpdir, overlap))
-
+    if len(arguments) > 0:
+        raise Exception("There are no genomes in the provided file")
     logging.getLogger().info(f"Annotating {len(arguments)} genomes using {cpu} cpus...")
     with Pool(processes = cpu) as p:
         bar = tqdm(range(len(arguments)), unit = "genome")
