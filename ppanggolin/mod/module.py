43bf37c1c7cfa2e047b9e946c65595d4d6fabc6a
jpjarnoux <arnoux.jeromepj@gmail.com>
2021-10-07 15:32:52 +0200

    Move module's functions to utils because they're used in searchCC now

--- ppanggolin/mod/module.py
+++ ppanggolin/mod/module.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-#coding:utf-8
+# coding:utf-8
 
-#default libraries
+# default libraries
 import logging
 import argparse
 import time
@@ -11,69 +11,46 @@ import subprocess
 from itertools import combinations
 from statistics import mean, median
 from collections import defaultdict
-#installed libraries
+
+# installed libraries
 from tqdm import tqdm
 import networkx as nx
-from gmpy2 import xmpz, popcount#pylint: disable=no-name-in-module
-#local libraries
+from gmpy2 import xmpz, popcount  # pylint: disable=no-name-in-module
+
+# local libraries
 from ppanggolin.pangenome import Pangenome
 from ppanggolin.region import Module
 from ppanggolin.formats import checkPangenomeInfo, writePangenome, ErasePangenome
-from ppanggolin.utils import mkOutdir, restricted_float
+from ppanggolin.utils import mkOutdir, restricted_float, add_gene, connected_components
 
-def connected_components(g, removed, weight):
-    """
-        Yields subgraphs of each connected component you get when filtering edges based on the given weight.
-    """
-    for v in g.nodes:
-        if v not in removed:
-            c = set(_plain_bfs(g, v, removed, weight))
-            yield c
-            removed.update(c)
-
-def _plain_bfs(g, source, removed, weight):
-    """A fast BFS node generator, copied from networkx then adapted to the current use case"""
-    nextlevel = {source}
-    while nextlevel:
-        thislevel = nextlevel
-        nextlevel = set()
-        for v in thislevel:
-            if v not in removed:
-                yield v
-                removed.add(v)
-
-                for n in g.neighbors(v):
-                    if n not in removed:
-                        edge_genes_v = g[v][n]["genes"][v]
-                        edge_genes_n = g[v][n]["genes"][n]
-                        #if the edge is indeed existent for most genes of both families, we use it
-                        if len(edge_genes_n) / len(g.nodes[n]["genes"]) >= weight and len(edge_genes_v) / len(g.nodes[v]["genes"]) >= weight:
-                            nextlevel.add(n)
 
 def checkPangenomeFormerModules(pangenome, force):
     """ checks pangenome status and .h5 files for former modules, delete them if allowed or raise an error """
     if pangenome.status["modules"] == "inFile" and force == False:
-        raise Exception("You are trying to detect modules on a pangenome which already has predicted modules. If you REALLY want to do that, use --force (it will erase modules previously predicted).")
+        raise Exception(
+            "You are trying to detect modules on a pangenome which already has predicted modules. If you REALLY want to do that, use --force (it will erase modules previously predicted).")
     elif pangenome.status["modules"] == "inFile" and force == True:
-        ErasePangenome(pangenome, modules = True)
+        ErasePangenome(pangenome, modules=True)
 
-def predictModules(pangenome, cpu, tmpdir, force=False, dup_margin=0.05, size=3, min_presence=2, transitive=4, jaccard=0.85, show_bar=True):
-    #check statuses and load info
+
+def predictModules(pangenome, cpu, tmpdir, force=False, dup_margin=0.05, size=3, min_presence=2, transitive=4,
+                   jaccard=0.85, show_bar=True):
+    # check statuses and load info
     checkPangenomeFormerModules(pangenome, force)
-    checkPangenomeInfo(pangenome, needAnnotations=True, needFamilies=True, needPartitions = True)
+    checkPangenomeInfo(pangenome, needAnnotations=True, needFamilies=True, needPartitions=True)
 
     ##compute the graph with transitive closure size provided as parameter
     start_time = time.time()
     logging.getLogger().info("Building the graph...")
     g = compute_mod_graph(pangenome.organisms, t=transitive, show_bar=show_bar)
-    logging.getLogger().info(f"Took {round(time.time() - start_time,2)} seconds to build the graph to find modules in")
+    logging.getLogger().info(f"Took {round(time.time() - start_time, 2)} seconds to build the graph to find modules in")
     logging.getLogger().info(f"There are {nx.number_of_nodes(g)} nodes and {nx.number_of_edges(g)} edges")
 
     start_time = time.time()
-    #get all multigenic gene families
+    # get all multigenic gene families
     multi = pangenome.get_multigenics(dup_margin, persistent=False)
 
-    #extract the modules from the graph
+    # extract the modules from the graph
     modules = compute_modules(g, multi, jaccard, min_presence, size=size)
 
     fams = set()
@@ -81,7 +58,7 @@ def predictModules(pangenome, cpu, tmpdir, force=False, dup_margin=0.05, size=3,
         fams |= mod.families
 
     logging.getLogger().info(f"There are {len(fams)} families among {len(modules)} modules")
-    logging.getLogger().info(f"Computing modules took {round(time.time() - start_time,2)} seconds")
+    logging.getLogger().info(f"Computing modules took {round(time.time() - start_time, 2)} seconds")
 
     pangenome.addModules(modules)
 
@@ -93,20 +70,6 @@ def predictModules(pangenome, cpu, tmpdir, force=False, dup_margin=0.05, size=3,
     pangenome.parameters["modules"]["jaccard"] = jaccard
     pangenome.parameters["modules"]["dup_margin"] = dup_margin
 
-def add_gene(obj, gene, fam_split=True):
-    if fam_split:
-        try:
-            obj["genes"][gene.family].add(gene)
-        except KeyError:
-            try:
-                obj["genes"][gene.family] = set([gene])
-            except KeyError:
-                obj["genes"] = {gene.family:set([gene])}
-    else:
-        try:
-            obj["genes"].add(gene)
-        except KeyError:
-            obj["genes"] = set([gene])
 
 def compute_mod_graph(organisms, t=1, show_bar=True):
     """
@@ -128,15 +91,16 @@ def compute_mod_graph(organisms, t=1, show_bar=True):
                 g.add_node(start_gene.family)
                 add_gene(g.nodes[start_gene.family], start_gene, fam_split=False)
                 for i, gene in enumerate(contig.genes):
-                    for j, a_gene in enumerate(contig.genes[i+1:i+t+2], start=i+1):
+                    for j, a_gene in enumerate(contig.genes[i + 1:i + t + 2], start=i + 1):
                         g.add_edge(gene.family, a_gene.family)
                         edge = g[gene.family][a_gene.family]
                         add_gene(edge, gene)
                         add_gene(edge, a_gene)
-                        if j == i+t+1 or i == 0:#if it's the last gene of the serie, or the first serie
+                        if j == i + t + 1 or i == 0:  # if it's the last gene of the serie, or the first serie
                             add_gene(g.nodes[a_gene.family], a_gene, fam_split=False)
 
     return g
+
 
 def compute_modules(g, multi, weight, min_fam, size):
     """
@@ -151,34 +115,44 @@ def compute_modules(g, multi, weight, min_fam, size):
     :param min_fam: the minimal number of presence under which the family is not considered
     :type min_fam: int
     """
-    #removing families with low presence
+    # removing families with low presence
     removed = set([fam for fam in g.nodes if len(fam.organisms) < min_fam])
 
     modules = set()
     c = 0
     for comp in connected_components(g, removed, weight):
-        if len(comp) >= size:#keep only the modules with at least 'size' non-multigenic genes.
-            if not any(fam.namedPartition == "persistent" and fam not in multi for fam in comp):#remove 'persistent' and non-multigenic modules
-                modules.add(Module(ID = c, families=comp))
+        if len(comp) >= size:  # keep only the modules with at least 'size' non-multigenic genes.
+            if not any(fam.namedPartition == "persistent" and fam not in multi for fam in
+                       comp):  # remove 'persistent' and non-multigenic modules
+                modules.add(Module(ID=c, families=comp))
                 c += 1
 
     return modules
 
+
 def launch(args):
     pangenome = Pangenome()
     pangenome.addFile(args.pangenome)
-    predictModules(pangenome = pangenome, cpu = args.cpu, tmpdir = args.tmpdir, force=args.force, dup_margin=args.dup_margin, size=args.size, min_presence=args.min_presence, transitive=args.transitive, jaccard=args.jaccard, show_bar=args.show_prog_bars)
+    predictModules(pangenome=pangenome, cpu=args.cpu, tmpdir=args.tmpdir, force=args.force, dup_margin=args.dup_margin,
+                   size=args.size, min_presence=args.min_presence, transitive=args.transitive, jaccard=args.jaccard,
+                   show_bar=args.show_prog_bars)
     writePangenome(pangenome, pangenome.file, args.force, show_bar=args.show_prog_bars)
 
 
 def moduleSubparser(subparser):
     parser = subparser.add_parser("module", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    optional = parser.add_argument_group(title = "Optional arguments")
-    optional.add_argument("--size", required=False, type=int, default=3, help = "Minimal number of gene family in a module")
-    optional.add_argument("--dup_margin", required=False, type=restricted_float, default=0.05, help = "minimum ratio of organisms in which the family must have multiple genes for it to be considered 'duplicated'")
-    optional.add_argument("-m","--min_presence", required=False, type=int, default=2, help = "Minimum number of times the module needs to be present in the pangenome to be reported. Increasing it will improve precision but lower sensitivity.")
-    optional.add_argument("-t", "--transitive",required=False, type=int, default = 4, help = "Size of the transitive closure used to build the graph. This indicates the number of non related genes allowed in-between two related genes. Increasing it will improve precision but lower sensitivity a little.")
-    optional.add_argument("-j", "--jaccard", required=False, type=restricted_float, default=0.85, help = "minimum jaccard similarity used to filter edges between gene families. Increasing it will improve precision but lower sensitivity a lot." )
-    required = parser.add_argument_group(title = "Required arguments", description = "One of the following arguments is required :")
-    required.add_argument('-p','--pangenome',  required=True, type=str, help="The pangenome .h5 file")
+    optional = parser.add_argument_group(title="Optional arguments")
+    optional.add_argument("--size", required=False, type=int, default=3,
+                          help="Minimal number of gene family in a module")
+    optional.add_argument("--dup_margin", required=False, type=restricted_float, default=0.05,
+                          help="minimum ratio of organisms in which the family must have multiple genes for it to be considered 'duplicated'")
+    optional.add_argument("-m", "--min_presence", required=False, type=int, default=2,
+                          help="Minimum number of times the module needs to be present in the pangenome to be reported. Increasing it will improve precision but lower sensitivity.")
+    optional.add_argument("-t", "--transitive", required=False, type=int, default=4,
+                          help="Size of the transitive closure used to build the graph. This indicates the number of non related genes allowed in-between two related genes. Increasing it will improve precision but lower sensitivity a little.")
+    optional.add_argument("-j", "--jaccard", required=False, type=restricted_float, default=0.85,
+                          help="minimum jaccard similarity used to filter edges between gene families. Increasing it will improve precision but lower sensitivity a lot.")
+    required = parser.add_argument_group(title="Required arguments",
+                                         description="One of the following arguments is required :")
+    required.add_argument('-p', '--pangenome', required=True, type=str, help="The pangenome .h5 file")
     return parser
--- ppanggolin/utils.py
+++ ppanggolin/utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-#coding:utf-8
+# coding:utf-8
 
-#default libraries
+# default libraries
 import gzip
 from io import TextIOWrapper
 import mmap
@@ -10,7 +10,8 @@ import os
 import numpy
 import argparse
 
-def jaccard_similarities(mat,jaccard_similarity_th):
+
+def jaccard_similarities(mat, jaccard_similarity_th):
     cols_sum = mat.getnnz(axis=0)
     ab = mat.T * mat
     # for rows
@@ -19,9 +20,10 @@ def jaccard_similarities(mat,jaccard_similarity_th):
     bb = cols_sum[ab.indices]
     similarities = ab.copy()
     similarities.data /= (aa + bb - ab.data)
-    similarities.data[similarities.data<jaccard_similarity_th] = 0
+    similarities.data[similarities.data < jaccard_similarity_th] = 0
     similarities.eliminate_zeros()
     return similarities
+
 
 def read_compressed_or_not(file_or_file_path):
     """
@@ -35,14 +37,15 @@ def read_compressed_or_not(file_or_file_path):
         try:
             file = open(file.name, "rb")
         except AttributeError:
-            return(file)
+            return (file)
     if file.read(2).startswith(b'\x1f\x8b'):
         file.seek(0)
-        return(TextIOWrapper(gzip.open(filename=file, mode="r")))
+        return (TextIOWrapper(gzip.open(filename=file, mode="r")))
     else:
         file.close()
         file = open(file.name, "r")
-        return(file)
+        return (file)
+
 
 def write_compressed_or_not(file_path, compress):
     """
@@ -52,6 +55,7 @@ def write_compressed_or_not(file_path, compress):
         return gzip.open(file_path + ".gz", mode="wt")
     else:
         return open(file_path, "w")
+
 
 def is_compressed(file_or_file_path):
     """
@@ -70,6 +74,7 @@ def is_compressed(file_or_file_path):
     file.close()
     return False
 
+
 def get_num_lines(file):
     fp = open(file, "r+")
     buf = mmap.mmap(fp.fileno(), 0)
@@ -78,17 +83,19 @@ def get_num_lines(file):
         lines += 1
     return lines
 
+
 def mkOutdir(output, force):
     if not os.path.exists(output):
         os.makedirs(output)
     elif not force:
         raise FileExistsError(f"{output} already exists. Use -f if you want to overwrite the files in the directory")
 
+
 def mkFilename(basename, output, force):
     """
         Returns a usable filename for a ppanggolin output file, or crashes.
     """
-    filename = Path(output + "/" + basename )
+    filename = Path(output + "/" + basename)
     if filename.suffix != ".h5":
         filename = filename.with_suffix(".h5")
 
@@ -98,14 +105,64 @@ def mkFilename(basename, output, force):
         raise FileExistsError(f"{filename.name} already exists. Use -f if you want to overwrite the file")
     return filename
 
+
 def restricted_float(x):
     x = float(x)
     if x < 0.0 or x > 1.0:
-        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+        raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]" % (x,))
     return x
+
 
 def min_one(x):
     x = int(x)
     if x < 1:
-        raise argparse.ArgumentTypeError("%r is inferior to 1"%(x,))
+        raise argparse.ArgumentTypeError("%r is inferior to 1" % (x,))
     return x
+
+
+def connected_components(g, removed, weight):
+    """
+        Yields subgraphs of each connected component you get when filtering edges based on the given weight.
+    """
+    for v in g.nodes:
+        if v not in removed:
+            c = set(_plain_bfs(g, v, removed, weight))
+            yield c
+            removed.update(c)
+
+
+def _plain_bfs(g, source, removed, weight):
+    """A fast BFS node generator, copied from networkx then adapted to the current use case"""
+    nextlevel = {source}
+    while nextlevel:
+        thislevel = nextlevel
+        nextlevel = set()
+        for v in thislevel:
+            if v not in removed:
+                yield v
+                removed.add(v)
+
+                for n in g.neighbors(v):
+                    if n not in removed:
+                        edge_genes_v = g[v][n]["genes"][v]
+                        edge_genes_n = g[v][n]["genes"][n]
+                        # if the edge is indeed existent for most genes of both families, we use it
+                        if len(edge_genes_n) / len(g.nodes[n]["genes"]) >= weight and len(edge_genes_v) / len(
+                                g.nodes[v]["genes"]) >= weight:
+                            nextlevel.add(n)
+
+
+def add_gene(obj, gene, fam_split=True):
+    if fam_split:
+        try:
+            obj["genes"][gene.family].add(gene)
+        except KeyError:
+            try:
+                obj["genes"][gene.family] = set([gene])
+            except KeyError:
+                obj["genes"] = {gene.family: set([gene])}
+    else:
+        try:
+            obj["genes"].add(gene)
+        except KeyError:
+            obj["genes"] = set([gene])
