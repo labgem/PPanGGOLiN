59102fa86534bb78a6f4ab55fdcf2f9741be8eac
jpjarnoux <arnoux.jeromepj@gmail.com>
2021-10-15 11:33:09 +0200

    Reformat & refactoring to be coherent with disable_prog_bar option

--- ppanggolin/workflow/panModule.py
+++ ppanggolin/workflow/panModule.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-#coding:utf-8
+# coding:utf-8
 
-#default libraries
+# default libraries
 import os
 import time
 import argparse
 import logging
 
-#local libraries
+# local libraries
 from ppanggolin.pangenome import Pangenome
 from ppanggolin.utils import mkFilename
 from ppanggolin.annotate import annotatePangenome, readAnnotations, getGeneSequencesFromFastas
@@ -18,102 +18,124 @@ from ppanggolin.nem.partition import partition
 from ppanggolin.formats import writePangenome, writeFlatFiles
 from ppanggolin.figures import drawTilePlot, drawUCurve
 from ppanggolin.info import printInfo
-from ppanggolin.RGP.genomicIsland import predictRGP
-from ppanggolin.RGP.spot import predictHotspots
 from ppanggolin.mod import predictModules
-### a global workflow that does everything in one go.
+
+
+"""a global workflow that does everything in one go."""
+
 
 def launch(args):
     pangenome = Pangenome()
     filename = mkFilename(args.basename, args.output, args.force)
-    if args.anno:#if the annotations are provided, we read from it
+    writing_time, anno_time, clust_time, mod_time, desc_time = (None, None, None, None, None)
+    if args.anno:  # if the annotations are provided, we read from it
         getSeq = True
         if args.clusters is not None:
             getSeq = False
         start_anno = time.time()
-        readAnnotations(pangenome, args.anno, cpu = args.cpu, getSeq = getSeq, show_bar=args.show_prog_bars)
-        annotime = time.time() - start_anno
+        readAnnotations(pangenome, args.anno, cpu=args.cpu, getSeq=getSeq, disable_bar=args.disable_prog_bar)
+        anno_time = time.time() - start_anno
         start_writing = time.time()
-        writePangenome(pangenome, filename, args.force, show_bar=args.show_prog_bars)
+        writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
         writing_time = time.time() - start_writing
         if args.clusters is None and pangenome.status["geneSequences"] == "No" and args.fasta is None:
-            raise Exception("The gff/gbff provided did not have any sequence informations, you did not provide clusters and you did not provide fasta file. Thus, we do not have the information we need to continue the analysis.")
+            raise Exception("The gff/gbff provided did not have any sequence informations, "
+                            "you did not provide clusters and you did not provide fasta file. "
+                            "Thus, we do not have the information we need to continue the analysis.")
 
         elif args.clusters is None and pangenome.status["geneSequences"] == "No" and args.fasta is not None:
             getGeneSequencesFromFastas(pangenome, args.fasta)
         start_clust = time.time()
         if args.clusters is not None:
-            readClustering(pangenome, args.clusters, show_bar=args.show_prog_bars)
+            readClustering(pangenome, args.clusters, disable_bar=args.disable_prog_bar)
 
-        elif args.clusters is None:#we should have the sequences here.
-            clustering(pangenome, args.tmpdir, args.cpu, defrag=not args.no_defrag, show_bar=args.show_prog_bars)
+        elif args.clusters is None:  # we should have the sequences here.
+            clustering(pangenome, args.tmpdir, args.cpu, defrag=not args.no_defrag, disable_bar=args.disable_prog_bar)
         clust_time = time.time() - start_clust
     elif args.fasta is not None:
         start_anno = time.time()
-        annotatePangenome(pangenome, args.fasta, args.tmpdir, args.cpu, show_bar=args.show_prog_bars)
-        annotime = time.time() - start_anno
+        annotatePangenome(pangenome, args.fasta, args.tmpdir, args.cpu, disable_bar=args.disable_prog_bar)
+        anno_time = time.time() - start_anno
         start_writing = time.time()
-        writePangenome(pangenome, filename, args.force, show_bar=args.show_prog_bars)
+        writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
         writing_time = time.time() - start_writing
         start_clust = time.time()
-        clustering(pangenome, args.tmpdir, args.cpu, defrag=not args.no_defrag, show_bar=args.show_prog_bars)
+        clustering(pangenome, args.tmpdir, args.cpu, defrag=not args.no_defrag, disable_bar=args.disable_prog_bar)
         clust_time = time.time() - start_clust
 
-    writePangenome(pangenome, filename, args.force, show_bar=args.show_prog_bars)
+    writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
     start_graph = time.time()
-    computeNeighborsGraph(pangenome, show_bar=args.show_prog_bars)
+    computeNeighborsGraph(pangenome, disable_bar=args.disable_prog_bar)
     graph_time = time.time() - start_graph
 
     start_part = time.time()
-    partition(pangenome, tmpdir = args.tmpdir, cpu = args.cpu, K=args.nb_of_partitions, show_bar=args.show_prog_bars)
+    partition(pangenome, tmpdir=args.tmpdir, cpu=args.cpu, K=args.nb_of_partitions, disable_bar=args.disable_prog_bar)
     part_time = time.time() - start_part
 
     start_writing = time.time()
-    writePangenome(pangenome, filename, args.force, show_bar=args.show_prog_bars)
+    writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
     writing_time = writing_time + time.time() - start_writing
 
     start_mods = time.time()
-    predictModules(pangenome = pangenome, cpu = args.cpu, tmpdir = args.tmpdir, show_bar=args.show_prog_bars)
+    predictModules(pangenome=pangenome, cpu=args.cpu, tmpdir=args.tmpdir, disable_bar=args.disable_prog_bar)
     mod_time = time.time() - start_mods
 
     start_writing = time.time()
-    writePangenome(pangenome, filename, args.force, show_bar=args.show_prog_bars)
+    writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
     writing_time = writing_time + time.time() - start_writing
 
     if args.rarefaction:
-        makeRarefactionCurve(pangenome,args.output, args.tmpdir, cpu=args.cpu, show_bar=args.show_prog_bars)
-    if len(pangenome.organisms) > 1 and len(pangenome.organisms) < 5000:
-        drawTilePlot(pangenome, args.output, nocloud = False if len(pangenome.organisms) < 500 else True)
+        makeRarefactionCurve(pangenome, args.output, args.tmpdir, cpu=args.cpu, disable_bar=args.disable_prog_bar)
+    if 1 < len(pangenome.organisms) < 5000:
+        drawTilePlot(pangenome, args.output, nocloud=False if len(pangenome.organisms) < 500 else True)
     drawUCurve(pangenome, args.output)
 
     start_desc = time.time()
-    writeFlatFiles(pangenome, args.output, args.cpu, csv = True, genePA=True, gexf=True, light_gexf = True, projection=True, json = True, stats = True, partitions = True, modules=True)
+    writeFlatFiles(pangenome, args.output, args.cpu, csv=True, genePA=True, gexf=True, light_gexf=True, projection=True,
+                   json=True, stats=True, partitions=True, modules=True)
     desc_time = time.time() - start_desc
 
-    logging.getLogger().info(f"Annotation took : {round(annotime,2)} seconds")
-    logging.getLogger().info(f"Clustering took : {round(clust_time,2)} seconds")
-    logging.getLogger().info(f"Building the graph took : {round(graph_time,2)} seconds")
-    logging.getLogger().info(f"Partitionning the pangenome took : {round(part_time,2)} seconds")
-    logging.getLogger().info(f"Predicting modules took : {round(start_mods, 2)} seconds")
-    logging.getLogger().info(f"Writing the pangenome data in HDF5 took : {round(writing_time,2)} seconds")
-    logging.getLogger().info(f"Writing descriptive files for the pangenome took : {round(desc_time,2)} seconds")
-    printInfo(filename, content = True)
+    logging.getLogger().info(f"Annotation took : {round(anno_time, 2)} seconds")
+    logging.getLogger().info(f"Clustering took : {round(clust_time, 2)} seconds")
+    logging.getLogger().info(f"Building the graph took : {round(graph_time, 2)} seconds")
+    logging.getLogger().info(f"Partitioning the pangenome took : {round(part_time, 2)} seconds")
+    logging.getLogger().info(f"Predicting modules took : {round(mod_time, 2)} seconds")
+    logging.getLogger().info(f"Writing the pangenome data in HDF5 took : {round(writing_time, 2)} seconds")
+    logging.getLogger().info(f"Writing descriptive files for the pangenome took : {round(desc_time, 2)} seconds")
+    printInfo(filename, content=True)
 
 
 def panModuleSubparser(subparser):
     parser = subparser.add_parser("panmodule", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    required = parser.add_argument_group(title = "Input arguments", description = "The possible input arguments :")
-    required.add_argument('--fasta',  required=False, type=str, help="A tab-separated file listing the organism names, and the fasta filepath of its genomic sequence(s) (the fastas can be compressed). One line per organism. This option can be used alone.")
-    required.add_argument('--anno', required=False, type=str, help="A tab-separated file listing the organism names, and the gff filepath of its annotations (the gffs can be compressed). One line per organism. This option can be used alone IF the fasta sequences are in the gff files, otherwise --fasta needs to be used.")
-    required.add_argument("--clusters",required=False, type=str, help = "a tab-separated file listing the cluster names, the gene IDs, and optionnally whether they are a fragment or not.")
+    required = parser.add_argument_group(title="Input arguments", description="The possible input arguments :")
+    required.add_argument('--fasta', required=False, type=str,
+                          help="A tab-separated file listing the organism names, "
+                               "and the fasta filepath of its genomic sequence(s) (the fastas can be compressed). "
+                               "One line per organism. This option can be used alone.")
+    required.add_argument('--anno', required=False, type=str,
+                          help="A tab-separated file listing the organism names, "
+                               "and the gff filepath of its annotations (the gffs can be compressed). "
+                               "One line per organism. This option can be used alone "
+                               "IF the fasta sequences are in the gff files, otherwise --fasta needs to be used.")
+    required.add_argument("--clusters", required=False, type=str,
+                          help="a tab-separated file listing the cluster names, the gene IDs, "
+                               "and optionally whether they are a fragment or not.")
 
-    optional = parser.add_argument_group(title = "Optional arguments")
-    optional.add_argument('-o','--output', required=False, type=str, default="ppanggolin_output"+time.strftime("_DATE%Y-%m-%d_HOUR%H.%M.%S", time.localtime())+"_PID"+str(os.getpid()), help="Output directory")
-    optional.add_argument("--basename",required = False, default = "pangenome", help = "basename for the output file")
-    optional.add_argument("--rarefaction", required=False, action = "store_true", help = "Use to compute the rarefaction curves (WARNING: can be time consumming)")
-    optional.add_argument("-K","--nb_of_partitions",required=False, default=-1, type=int, help = "Number of partitions to use. Must be at least 2. If under 2, it will be detected automatically.")
-    optional.add_argument("--defrag", required=False, action = "store_true", help = argparse.SUPPRESS)##This ensures compatibility with workflows built with the old option "defrag" when it was not the default
-    optional.add_argument("--no_defrag",required=False, action="store_true", help = "DO NOT Realign gene families to link fragments with their non-fragmented gene family.")
+    optional = parser.add_argument_group(title="Optional arguments")
+    optional.add_argument('-o', '--output', required=False, type=str,
+                          default="ppanggolin_output" + time.strftime("_DATE%Y-%m-%d_HOUR%H.%M.%S",
+                                                                      time.localtime()) + "_PID" + str(os.getpid()),
+                          help="Output directory")
+    optional.add_argument("--basename", required=False, default="pangenome", help="basename for the output file")
+    optional.add_argument("--rarefaction", required=False, action="store_true",
+                          help="Use to compute the rarefaction curves (WARNING: can be time consuming)")
+    optional.add_argument("-K", "--nb_of_partitions", required=False, default=-1, type=int,
+                          help="Number of partitions to use. Must be at least 2. "
+                               "If under 2, it will be detected automatically.")
+    optional.add_argument("--defrag", required=False, action="store_true", help=argparse.SUPPRESS)
+    # This ensures compatibility with workflows built with the old option "defrag" when it was not the default
+    optional.add_argument("--no_defrag", required=False, action="store_true",
+                          help="DO NOT Realign gene families to link fragments with their non-fragmented gene family.")
 
     return parser
--- ppanggolin/workflow/workflow.py
+++ ppanggolin/workflow/workflow.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-#coding:utf-8
+# coding:utf-8
 
-#default libraries
+# default libraries
 import os
 import time
 import argparse
 
-#local libraries
+# local libraries
 from ppanggolin.pangenome import Pangenome
 from ppanggolin.utils import mkFilename, min_one
 from ppanggolin.annotate import annotatePangenome, readAnnotations, getGeneSequencesFromFastas
@@ -17,64 +17,91 @@ from ppanggolin.nem.partition import partition
 from ppanggolin.formats import writePangenome, writeFlatFiles
 from ppanggolin.figures import drawTilePlot, drawUCurve
 from ppanggolin.info import printInfo
-### a global workflow that does everything in one go.
+
+
+""" a global workflow that does everything in one go. """
+
 
 def launch(args):
     pangenome = Pangenome()
     filename = mkFilename(args.basename, args.output, args.force)
-    if args.anno:#if the annotations are provided, we read from it
+    if args.anno:  # if the annotations are provided, we read from it
         getSeq = True
         if args.clusters is not None:
             getSeq = False
-        readAnnotations(pangenome, args.anno, cpu = args.cpu, getSeq = getSeq, show_bar=args.show_prog_bars)
-        writePangenome(pangenome, filename, args.force)
+        readAnnotations(pangenome, args.anno, cpu=args.cpu, getSeq=getSeq, disable_bar=args.disable_prog_bar)
+        writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
         if args.clusters is None and pangenome.status["geneSequences"] == "No" and args.fasta is None:
-            raise Exception("The gff/gbff provided did not have any sequence informations, you did not provide clusters and you did not provide fasta file. Thus, we do not have the information we need to continue the analysis.")
+            raise Exception("The gff/gbff provided did not have any sequence informations, "
+                            "you did not provide clusters and you did not provide fasta file. "
+                            "Thus, we do not have the information we need to continue the analysis.")
 
         elif args.clusters is None and pangenome.status["geneSequences"] == "No" and args.fasta is not None:
             getGeneSequencesFromFastas(pangenome, args.fasta)
 
         if args.clusters is not None:
-            readClustering(pangenome, args.clusters, show_bar=args.show_prog_bars)
+            readClustering(pangenome, args.clusters, disable_bar=args.disable_prog_bar)
 
-        elif args.clusters is None:#we should have the sequences here.
-            clustering(pangenome, tmpdir = args.tmpdir, cpu = args.cpu, defrag = not args.no_defrag, show_bar=args.show_prog_bars)
+        elif args.clusters is None:  # we should have the sequences here.
+            clustering(pangenome, tmpdir=args.tmpdir, cpu=args.cpu, defrag=not args.no_defrag,
+                       disable_bar=args.disable_prog_bar)
     elif args.fasta is not None:
         pangenome = Pangenome()
-        annotatePangenome(pangenome, args.fasta, args.tmpdir, args.cpu, contig_filter=args.contig_filter, show_bar=args.show_prog_bars)
-        writePangenome(pangenome, filename, args.force,show_bar=args.show_prog_bars)
-        clustering(pangenome, tmpdir = args.tmpdir,cpu = args.cpu, defrag = not args.no_defrag,show_bar=args.show_prog_bars)
+        annotatePangenome(pangenome, args.fasta, args.tmpdir, args.cpu, contig_filter=args.contig_filter,
+                          disable_bar=args.disable_prog_bar)
+        writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
+        clustering(pangenome, tmpdir=args.tmpdir, cpu=args.cpu, defrag=not args.no_defrag,
+                   disable_bar=args.disable_prog_bar)
 
-    computeNeighborsGraph(pangenome,show_bar=args.show_prog_bars)
+    computeNeighborsGraph(pangenome, disable_bar=args.disable_prog_bar)
 
-    partition(pangenome, tmpdir = args.tmpdir, cpu = args.cpu, K=args.nb_of_partitions,show_bar=args.show_prog_bars)
-    writePangenome(pangenome, filename, args.force,show_bar=args.show_prog_bars)
+    partition(pangenome, tmpdir=args.tmpdir, cpu=args.cpu, K=args.nb_of_partitions, disable_bar=args.disable_prog_bar)
+    writePangenome(pangenome, filename, args.force, disable_bar=args.disable_prog_bar)
 
     if args.rarefaction:
-        makeRarefactionCurve(pangenome,args.output, args.tmpdir, cpu=args.cpu,show_bar=args.show_prog_bars)
-    if len(pangenome.organisms) > 1 and len(pangenome.organisms) < 5000:
-        drawTilePlot(pangenome, args.output, nocloud = False if len(pangenome.organisms) < 500 else True)
+        makeRarefactionCurve(pangenome, args.output, args.tmpdir, cpu=args.cpu, disable_bar=args.disable_prog_bar)
+    if 1 < len(pangenome.organisms) < 5000:
+        drawTilePlot(pangenome, args.output, nocloud=False if len(pangenome.organisms) < 500 else True)
     drawUCurve(pangenome, args.output)
 
-    writeFlatFiles(pangenome, args.output, args.cpu, csv = True, genePA=True, gexf=True, light_gexf = True, projection=True, json = True, stats = True, partitions = True)
+    writeFlatFiles(pangenome, args.output, args.cpu, csv=True, genePA=True, gexf=True, light_gexf=True, projection=True,
+                   json=True, stats=True, partitions=True)
 
-    printInfo(filename, content = True)
+    printInfo(filename, content=True)
 
 
 def workflowSubparser(subparser):
     parser = subparser.add_parser("workflow", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    required = parser.add_argument_group(title = "Input arguments", description = "The possible input arguments :")
-    required.add_argument('--fasta',  required=False, type=str, help="A tab-separated file listing the organism names, and the fasta filepath of its genomic sequence(s) (the fastas can be compressed). One line per organism. This option can be used alone.")
-    required.add_argument('--anno', required=False, type=str, help="A tab-separated file listing the organism names, and the gff filepath of its annotations (the gffs can be compressed). One line per organism. This option can be used alone IF the fasta sequences are in the gff files, otherwise --fasta needs to be used.")
-    required.add_argument("--clusters",required=False, type=str, help = "a tab-separated file listing the cluster names, the gene IDs, and optionnally whether they are a fragment or not.")
+    required = parser.add_argument_group(title="Input arguments", description="The possible input arguments :")
+    required.add_argument('--fasta', required=False, type=str,
+                          help="A tab-separated file listing the organism names, "
+                               "and the fasta filepath of its genomic sequence(s) (the fastas can be compressed). "
+                               "One line per organism. This option can be used alone.")
+    required.add_argument('--anno', required=False, type=str,
+                          help="A tab-separated file listing the organism names, and the gff filepath "
+                               "of its annotations (the gffs can be compressed). One line per organism. "
+                               "This option can be used alone IF the fasta sequences are in the gff files, "
+                               "otherwise --fasta needs to be used.")
+    required.add_argument("--clusters", required=False, type=str,
+                          help="a tab-separated file listing the cluster names, the gene IDs, "
+                               "and optionally whether they are a fragment or not.")
 
-    optional = parser.add_argument_group(title = "Optional arguments")
-    optional.add_argument('-o','--output', required=False, type=str, default="ppanggolin_output"+time.strftime("_DATE%Y-%m-%d_HOUR%H.%M.%S", time.localtime())+"_PID"+str(os.getpid()), help="Output directory")
-    optional.add_argument("--basename",required = False, default = "pangenome", help = "basename for the output file")
-    optional.add_argument("--rarefaction", required=False, action = "store_true", help = "Use to compute the rarefaction curves (WARNING: can be time consumming)")
-    optional.add_argument("-K","--nb_of_partitions",required=False, default=-1, type=int, help = "Number of partitions to use. Must be at least 2. If under 2, it will be detected automatically.")
-    optional.add_argument("--defrag", required=False, action = "store_true", help = argparse.SUPPRESS)##This ensures compatibility with workflows built with the old option "defrag" when it was not the default
-    optional.add_argument("--no_defrag",required=False, action="store_true", help = "DO NOT Realign gene families to link fragments with their non-fragmented gene family.")
-    optional.add_argument("--contig_filter",required=False, default=1, type=min_one, help = argparse.SUPPRESS)
+    optional = parser.add_argument_group(title="Optional arguments")
+    optional.add_argument('-o', '--output', required=False, type=str,
+                          default="ppanggolin_output" + time.strftime("_DATE%Y-%m-%d_HOUR%H.%M.%S",
+                                                                      time.localtime()) + "_PID" + str(os.getpid()),
+                          help="Output directory")
+    optional.add_argument("--basename", required=False, default="pangenome", help="basename for the output file")
+    optional.add_argument("--rarefaction", required=False, action="store_true",
+                          help="Use to compute the rarefaction curves (WARNING: can be time consuming)")
+    optional.add_argument("-K", "--nb_of_partitions", required=False, default=-1, type=int,
+                          help="Number of partitions to use. Must be at least 2. If under 2, "
+                               "it will be detected automatically.")
+    optional.add_argument("--defrag", required=False, action="store_true",
+                          help=argparse.SUPPRESS)
+    # This ensures compatibility with workflows built with the old option "defrag" when it was not the default
+    optional.add_argument("--no_defrag", required=False, action="store_true",
+                          help="DO NOT Realign gene families to link fragments with their non-fragmented gene family.")
+    optional.add_argument("--contig_filter", required=False, default=1, type=min_one, help=argparse.SUPPRESS)
     return parser
