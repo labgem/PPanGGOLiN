c6b4ee15af43ccfe1a46f885a8ba4038768ba333
JeanMainguy <jean.mainguy@outlook.fr>
2026-03-17 17:46:34 +0100

    apply black

--- ppanggolin/context/searchGeneContext.py
+++ ppanggolin/context/searchGeneContext.py
@@ -764,7 +764,9 @@ def launch(args: argparse.Namespace):
     pangenome = Pangenome()
     pangenome.add_file(args.pangenome)
 
-    is_translation_table_specified = "translation_table" in getattr(args, "specified_args", set())
+    is_translation_table_specified = "translation_table" in getattr(
+        args, "specified_args", set()
+    )
     translation_table = check_translation_table_to_use(
         pangenome.status["translation_table"],
         is_translation_table_specified,
